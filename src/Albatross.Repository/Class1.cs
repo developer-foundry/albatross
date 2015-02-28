5762c2308c40506db0a4aff00593ba9333edc461
Keith La Force <klaforce@gmail.com>
2015-02-27 19:48:37 -0500

    Revert "Changing domain,services,repository to regular class library not ASP.NET class library."
    This reverts commit 6566ab42a088c37554f80073fbdac2e2066b577d.

---
+++ src/Albatross.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Albatross.Repository
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+}
---
+++ src/Albatross.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Albatross.Services
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+}
