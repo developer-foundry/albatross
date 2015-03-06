23940a1a9f2eb745009f5664fadb5ed320f0f70a
Keith La Force <klaforce@gmail.com>
2015-03-06 16:37:47 -0500

    starting to add api

---
+++ Albatross.Example.API/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Microsoft.Practices.Unity;
+using Owin;
+using Microsoft.Owin.Cors;
+using Unity.SelfHostWebApiOwin;
+
+
+namespace SignalRSelfHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Startup.StartServer();
+        }
+    }
+}
---
+++ Albatross.Example.API/Unity.SelfHostWebApiOwin/UnityHelpers.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Reflection;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Practices.Unity;
+using System.Linq;
+using SignalRSelfHost;
+using SignalR_Rx_SelfHost_Example;
+using SignalR_Rx_SelfHost_Example.Unity.SelfHostWebApiOwin;
+
+namespace Unity.SelfHostWebApiOwin
+{
+    public static class UnityHelpers
+    {
+        #region Unity Container
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+        #endregion
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.RegisterType(typeof(Startup));
+            /*container.RegisterType<DbContext, ApplicationDbContext>(new HierarchicalLifetimeManager()); 
+            container.RegisterType<IRepository, EntityFrameworkRepository>(new TransientLifetimeManager());
+            container.RegisterType<IObservableRepository<Stock>, EntityFrameworkObservableRepository<Stock>>(new TransientLifetimeManager());
+            container.RegisterType<ITickerService, TickerService>(new TransientLifetimeManager());*/
+            container.RegisterType<MyHub, MyHub>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IHubActivator, UnityHubActivator>(new ContainerControlledLifetimeManager());
+        }
+
+    }
+}
